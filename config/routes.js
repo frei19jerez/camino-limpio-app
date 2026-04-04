b8df7a197feceff4d2435fd4b0fe0414c5cb0317
Freimel Jerez <frei19jerez@gmail.com>
2026-04-04 17:39:45 -0500

    Prueba ruta raiz render

--- config/routes.js
+++ config/routes.js
@@ -1,43 +1,13 @@
-/**
- * Route Mappings
- * (sails.config.routes)
- *
- * Your routes tell Sails what to do each time it receives a request.
- *
- * For more information on configuring custom routes, check out:
- * https://sailsjs.com/anatomy/config/routes-js
- */
-
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+  '/': async function(req, res) {
+    return res.send('Servidor funcionando en Render 🔥');
+  },
 
-  '/': { view: 'pages/homepage' },
+  'POST /lavado/crear': 'LavadoController.crear',
 
-'POST /lavado/crear': 'LavadoController.crear',
+  'GET /recibo/:id': 'LavadoController.recibo',
 
-'GET /recibo/:id': 'LavadoController.recibo',
-
-'GET /panel': 'LavadoController.panel',
-
-
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
-
+  'GET /panel': 'LavadoController.panel',
 
 };
